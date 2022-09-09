3538c46ec38ccd536befaaf55c5781977044f2de
Lucas Andrade <lucas@Lucass-MacBook-Pro.local>
2022-09-09 12:52:08 -0300

    feat: Add orderBy to json parameters

--- functions/getBrazilianStates.py
+++ functions/getBrazilianStates.py
@@ -1,14 +1,16 @@
 from functools import partial
 import json
-from utils import filterValuesFromDict, getStatesByRegion
+from utils import filterValuesFromDict, getStatesByRegion, orderBy
 
 with open('data.json', encoding='utf-8') as file:
     data = json.load(file)
 
 def exec(where):
     response = data
-    if where['region']:
-        response = getStatesByRegion.exec(where['region'], response)
-    if where['fields']:
-        response = map(partial(filterValuesFromDict.exec, values=where['fields']), response)
+    if where.get('orderBy'):
+        response = orderBy.exec(response, where.get('orderBy'))
+    if where.get('region'):
+        response = getStatesByRegion.exec(where.get('region'), response)
+    if where.get('fields'):
+        response = map(partial(filterValuesFromDict.exec, values=where.get('fields')), response)
     return json.dumps(list(response), ensure_ascii=False)
