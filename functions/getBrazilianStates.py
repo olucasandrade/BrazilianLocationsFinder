78a29c5a940add6b10792f10816994305c187b70
Lucas Andrade <lucas@Lucass-MacBook-Pro.local>
2022-09-09 12:17:09 -0300

    feat: Add region filter to route

--- functions/getBrazilianStates.py
+++ functions/getBrazilianStates.py
@@ -1,12 +1,14 @@
 from functools import partial
 import json
-from utils import filterValuesFromDict
+from utils import filterValuesFromDict, getStatesByRegion
 
 with open('data.json', encoding='utf-8') as file:
     data = json.load(file)
 
 def exec(where):
     response = data
+    if where['region']:
+        response = getStatesByRegion.exec(where['region'], response)
     if where['fields']:
         response = map(partial(filterValuesFromDict.exec, values=where['fields']), response)
     return json.dumps(list(response), ensure_ascii=False)
