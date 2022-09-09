29dd62abc34333e135e6776c1f7c57dfddb3807c
Lucas Andrade <lucas@Lucass-MacBook-Pro.local>
2022-09-09 11:58:39 -0300

    feat: Create first condition for getBrazilianCities

--- functions/getBrazilianCities.py
+++ functions/getBrazilianCities.py
@@ -1,5 +1,6 @@
 import json
 from functools import reduce
+from utils import getCitiesByState
 
 with open('data.json', encoding='utf-8') as file:
     data = json.load(file)
@@ -8,6 +9,8 @@ def getCities(obj):
     return obj['cities']
 
 def exec(where):
+    if where['state']:
+        return getCitiesByState.exec(data, where['state'])
     arr = map(getCities, data)
     flatArr = reduce(lambda a,b:a+b, arr)
     return json.dumps(list(flatArr), ensure_ascii=False)
