f289bd593ff340553964fe8ce752f5a4c7e626e3
Lucas Andrade <lucas@Lucass-MacBook-Pro.local>
2022-09-09 11:58:51 -0300

    feat: Create first condition for getBrazilianStates

--- functions/getBrazilianStates.py
+++ functions/getBrazilianStates.py
@@ -1,7 +1,12 @@
+from functools import partial
 import json
+from utils import filterValuesFromDict
 
 with open('data.json', encoding='utf-8') as file:
     data = json.load(file)
 
 def exec(where):
-    return json.dumps(data, ensure_ascii=False)
+    response = data
+    if where['fields']:
+        response = map(partial(filterValuesFromDict.exec, values=where['fields']), response)
+    return json.dumps(list(response), ensure_ascii=False)
